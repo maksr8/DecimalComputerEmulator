4eb5c14e9538ee4a0ea78463d17db2772e6e5b9e
maksr8 <158086853+maksr8@users.noreply.github.com>
2026-03-23 01:18:58 +0200

    Make cpu stats changeable

--- src/gui/CpuStats.h
+++ src/gui/CpuStats.h
@@ -3,16 +3,32 @@
 
 class CpuStats : public QObject {
     Q_OBJECT
-        Q_PROPERTY(int alu READ alu CONSTANT)
-        Q_PROPERTY(int memory READ memory CONSTANT)
-        Q_PROPERTY(int control READ control CONSTANT)
-        Q_PROPERTY(int io READ io CONSTANT)
+        Q_PROPERTY(int alu READ alu NOTIFY statsChanged)
+        Q_PROPERTY(int memory READ memory NOTIFY statsChanged)
+        Q_PROPERTY(int control READ control NOTIFY statsChanged)
+        Q_PROPERTY(int io READ io NOTIFY statsChanged)
 
 public:
-    explicit CpuStats(QObject* parent = nullptr) : QObject(parent) {}
+    explicit CpuStats(QObject* parent = nullptr) :
+        QObject(parent), _alu(0), _memory(0), _control(0), _io(0)
+    {}
 
-    int alu() const { return 42; }
-    int memory() const { return 28; }
-    int control() const { return 15; }
-    int io() const { return 15; }
+    int alu() const { return _alu; }
+    int memory() const { return _memory; }
+    int control() const { return _control; }
+    int io() const { return _io; }
+
+    void updateStats(int alu, int mem, int ctrl, int io) {
+        _alu = alu;
+        _memory = mem;
+        _control = ctrl;
+        _io = io;
+        emit statsChanged();
+    }
+
+signals:
+    void statsChanged();
+
+private:
+    int _alu, _memory, _control, _io;
 };
